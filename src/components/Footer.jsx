7d4a3aec589c32bd621af1ff4a8dc0e4ba5241ce
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-11-15 18:23:21 +0500

    update the code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,25 +16,25 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='w-full h-auto bg-[#000000] text-white  p-10'>
-            <div className='flex flex-wrap justify-around items-center text-center md:text-start gap-5 my-10'>
-                <div className='space-y-5 w-48'>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3'>
+                <div className='space-y-5 '>
                     <h1 className=' text-2xl font-semibold '>Exclusive</h1>
                     <p className='text-lg font-semibold'>Subcribe</p>
                     <p>Get 10% off your first order</p>
                     <div className='relative'>
                         <input type="text" name="" id="" className=' border  h-8 rounded-md p-3' placeholder='Enter your name ' />
-                        <IoSendSharp className='absolute right-5 top-2' />
+                        <IoSendSharp className='absolute right-10 top-2' />
                     </div>
 
 
                 </div>
-                <div className='space-y-6 w-48'>
+                <div className='space-y-6  '>
                     <h1 className='text-lg font-semibold'>Support</h1>
                     <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
                     <p>exclusive@gmail.com</p>
                     <p>+88015-88888-9999</p>
                 </div>
-                <div className='space-y-3 w-48'>
+                <div className='space-y-3 '>
                     <h1 className='text-lg font-semibold'>Account</h1>
                     <div>
                         <Link to="/useraccount">
@@ -62,14 +62,14 @@ const Footer = () => {
 
 
                 </div>
-                <div className='space-y-5 w-48'>
+                <div className='space-y-5  '>
                     <h1 className='text-lg font-semibold'>Quick Link</h1>
                     <p>Privacy Policy</p>
                     <p>Terms Of Use</p>
                     <p>FAQ</p>
                     <p>Contact</p>
                 </div>
-                <div className='space-y-4 w-48'>
+                <div className='space-y-4 '>
                     <h1 className='text-lg font-semibold'>Download App</h1>
                     <p>Save $3 with App New User Only</p>
                     <div className='flex justify-center items-center gap-3'>
